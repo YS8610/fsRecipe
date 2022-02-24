fd895bcbe38fb7a6b55c9625ba6bc53bcf5f17a1
YS_workPC <ong_ys86@yahoo.com.sg>
2022-02-24 20:55:48 +0800

    use relative path instead of absolute path for service

--- ngRecipe/src/app/recipe.service.ts
+++ ngRecipe/src/app/recipe.service.ts
@@ -10,23 +10,23 @@ export class RecipeService {
   recipesList: Recipe[] = [];
 
   // URL = "https://recipetfip.herokuapp.com/"
-  URL = "http://127.0.0.1:8080/"
+  // URL = "http://127.0.0.1:8080/"
 
   constructor(
     private http:HttpClient
   ) { }
 
   getAllRecipes(){
-    return this.http.get<Recipe[]>(this.URL+ "api/recipes")
+    return this.http.get<Recipe[]>("api/recipes")
   }
 
   getRecipe( recipeId:string){
-    const urlid = this.URL +"api/recipe/" + recipeId +"/";
+    const urlid = "api/recipe/" + recipeId +"/";
     return this.http.get<Recipe>(urlid)
   }
 
   setNewRecipe(recipe:Recipe){
-    const urlpost = this.URL+"api/recipe"
+    const urlpost = "api/recipe"
     return this.http.post<Recipe>(urlpost,recipe)
   }
 }
